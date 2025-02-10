b5937d0c0ecc2d52c5a52ac708fce2ab62208a73
Azeemnadaf001 <131427107+Azeemnadaf001@users.noreply.github.com>
2025-02-10 12:16:05 +0000

    strings

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = true;
+
+// console.log(typeof score);
+
+let convertInNumber = Number(score);
+
+// console.log(typeof convertInNumber);
+// console.log(convertInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+//  null => 0
+// undefined => NaN
+// true => 1 ; / false => 0;
+
+let isLoggedIn = "";
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 1 = true ; 0 => false
+// "" => false
+// "azeem" => true
+
+let someNumber = 33;
+
+let someString = String(someNumber);
+
+console.log(typeof someString);
+console.log(someString);
