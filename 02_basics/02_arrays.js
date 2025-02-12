60b87c84d88170b521a5f552116675f622124da4
Azeemnadaf001 <131427107+Azeemnadaf001@users.noreply.github.com>
2025-02-12 11:09:21 +0000

    arrays2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,44 @@
+// arrays part - 2
+
+const onePiece_heroes = ["Luffy", "Sanji", "Zoro", "Red-shanks"];
+
+// console.log(onePiece_heroes, "\n");
+
+const onePiece_villains = ["Akainu", "Blackbeard", "Kaido", "Big-mom"];
+
+const onePiece_dead = ["Ace", "Gol D Roger", "Whitebeard"];
+
+// console.log(onePiece_villains, "\n");
+
+// // onePiece_heroes.push(onePiece_villains);
+
+// // console.log(onePiece_heroes, "\n");
+
+// const onePiece_characters = onePiece_heroes.concat(onePiece_villains);
+
+// console.log(onePiece_characters);
+
+// const all_onePiece = [
+//   ...onePiece_heroes,
+//   ...onePiece_villains,
+//   ...onePiece_dead,
+// ];
+
+// console.log(all_onePiece);
+
+// const newArr = [1, 2, 3, 4, [5, 6, 7], 8, [9, 10, [11, 12, [13]]]];
+
+// const flatArr = newArr.flat(Infinity);
+
+// console.log(flatArr);
+
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh"));
+
+// console.log(Array.from({ name: "azeem" }));
+
+let a = 100;
+let b = 200;
+let c = 400;
+
+console.log(Array.of(a, b, c));
